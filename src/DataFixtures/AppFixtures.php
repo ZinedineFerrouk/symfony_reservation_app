6800679d0d8c55c657e5f98de9a7f92aca44e63d
ZinedineFerrouk <zinedine27000@gmail.com>
2022-12-16 11:59:45 +0100

    Fixtures ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,17 +2,34 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements DependentFixtureInterface
 {
+    // public function __construct(private UserPasswordHasherInterface $userEncoderInterface) {}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // $admin = (new User())->setEmail('admin@gmail.com')->setRoles(['ROLE_ADMIN', 'ROLE_SUPER_ADMIN', 'ROLE_USER']);
+        // $user1 = (new User())->setEmail('user@gmail.com')->setRoles(['ROLE_USER']);
+        
+        // for ($i=1; $i < 10; $i++) { 
+        //     $user[$i] = (new User())->setEmail('user'. $i .'@gmail.com')->setRoles(['ROLE_USER', 'ROLE_SUPER_ADMIN']);   
+        //     $user[$i]->setPassword($this->userEncoderInterface->hashPassword($user[$i], 'test1234'));
+        //     $manager->persist($user[$i]);
+        // }
 
-        $manager->flush();
+        // $admin->setPassword($this->userEncoderInterface->hashPassword($admin, 'test1234'));
+        // $user1->setPassword($this->userEncoderInterface->hashPassword($user1, 'test1234'));
+
+        // $manager->persist($admin);
+        // $manager->persist($user1);
+
+        // $manager->flush();
     }
 
     public function getDependencies()
