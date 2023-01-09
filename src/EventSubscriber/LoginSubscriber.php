e991fa793d83c808d41a4081b71e7a62e9a6a8e3
ZinedineFerrouk <zinedine27000@gmail.com>
2023-01-09 15:03:07 +0100

    Fix: Entity call user in subscriber

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -2,24 +2,27 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Doctrine\ORM\EntityManagerInterface;
-// use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
-    public function __construct(EntityManagerInterface $entityManager){
-        // $this->session = $session;
+    private $session;
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, Session $session){
+        $this->session = $session;
         $this->entityManager = $entityManager;
     }
     
     public function onLoginSuccessEvent(LoginSuccessEvent $event): void
     {
-        // $this->session->getFlashBag()->add('success', 'Heureux de vous revoir par ici !');
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['id' => $event->getUser()->getId()]);
         $user->setVisites($user->getVisites() + 1);
-        dd($user);
+        $this->session->getFlashBag()->add('success', 'Heureux de vous revoir par ici !');
     }
 
     public static function getSubscribedEvents(): array
